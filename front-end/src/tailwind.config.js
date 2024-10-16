f7d5555bfcdf083063ea4c97b80952e41fc66407
Songdddd <hs4801@nyu.edu>
2024-10-15 21:24:41 -0400

    set up react framework

--- front-end/src/tailwind.config.js
+++ front-end/src/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        danger: '#e3342f',
+      },
+    },
   },
-  plugins: [],
-}
+
+  "scripts": {
+    "build:css": "tailwindcss -i ./src/styles/tailwind.css -o ./dist/output.css --watch"
+  }
+
+};
+
 
