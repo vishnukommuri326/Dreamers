4346bb8e4c8d58e9e86cb874916cc98bf0fcff99
kahmeeah <kao8144@nyu.edu>
2024-10-23 00:46:24 -0400

    refactor: move code for home page to home.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,41 +1,13 @@
 import React, { useState } from 'react';
 import '../src/assets/styles/App.css';
-import logo from '../src/assets/images/logo.svg';
-import map from '../src/assets/images/map.png';
 
-function App() {
-  const [scale, setScale] = useState(1);
-
-  const handleZoom = (event) => {
-    const zoomFactor = event.deltaY < 0 ? 0.1 : -0.1;
-    setScale((prevScale) => Math.max(1, prevScale + zoomFactor));
-  };
+const App = props => {
 
   return (
     <div className="App">
-      {/* Header */}
-      <header className="App-header">
-        <img src={logo} alt="Logo" className="logo" />
-        <button className="hamburger-menu" onClick={() => alert('Menu clicked!')}>
-          &#9776;
-        </button>
-      </header>
 
-      {/* Map Section */}
-      <section className="map-container">
-        <div className="map-settings">
-          <button>Map Settings</button>
-        </div>
-        <div className="map" onWheel={handleZoom}>
-          <img
-            src = {map}
-            alt="Map"
-            className="zoomable-map"
-            style={{ transform: `scale(${scale})` }}
-          />
-        </div>
-      </section>
     </div>
+
   );
 }
 
