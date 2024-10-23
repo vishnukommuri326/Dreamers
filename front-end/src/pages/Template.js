07b7f10f9f4646da510465e407d27dc3984a9023
kahmeeah <kao8144@nyu.edu>
2024-10-23 00:45:06 -0400

    feat: add template for app pages

--- front-end/src/pages/Template.js
+++ front-end/src/pages/Template.js
@@ -1,1 +1,15 @@
-// use this file to create new pages as needed
+/**
+ * A React component that represents a single page of the app.
+ * Use this file to create new pages as needed
+ * Don't forget to add the new route to App.js
+ */
+const Template = props => {
+  return (
+    <>
+      <h1>Start writing here!</h1>
+    </>
+  )
+}
+
+// make this component available to be imported into any other file
+export default Template
