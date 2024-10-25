94bda15589e7b064c0dc35fd23694ebb59e67635
Mohamed Alremeithi <alremaithi2212@icloud.com>
2024-10-25 14:12:17 -0400

    Testing with Pull Requests, modified about page

--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -3,10 +3,16 @@
  * Use this file to create new pages as needed
  * Don't forget to add the new route to App.js
  */
+
+
 const About = props => {
     return (
       <div>
-        <h1>Start writing here!</h1>
+        <h1>About Us</h1>
+
+        While We Were Dreaming is a location-based platform designed to connect NYU students through the shared memories they create on campus. By pinning personal experiences to a dynamic map, students can reflect on key moments of their college journey and explore the memories of others enhanced by anonymity. This platform fosters a sense of community and nostalgia, bridging the emotional gap often felt in an open-city campus like NYU. The map becomes a collective memory, capturing the dreams and experiences of past, present, and future students.
+
+
       </div>
     )
   }
