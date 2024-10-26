5133ab7a64dd34a4e35912429c4185fddbd1de34
kahmeeah <kao8144@nyu.edu>
2024-10-26 16:57:36 -0400

    style: remove padding between title

--- front-end/src/components/card.js
+++ front-end/src/components/card.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Card({ title, children }) {
   return (
     <div className="bg-purpleLight text-purpleDark shadow-md rounded-lg p-4">
-      <h2 className="text-2xl font-bold mb-4">{title}</h2>
-      <div>{children}</div>
+      {title && <h2 className="text-2xl font-bold p-1">{title}</h2>}
+      <div className='m-1'>
+        {children}
+      </div>
     </div>
   );
 }
