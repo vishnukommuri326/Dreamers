8ca580451c0911e626131ff5b9df291c38f1ae1a
kahmeeah <kao8144@nyu.edu>
2024-10-30 19:31:16 -0400

    refactor: remove unused code

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -16,83 +16,9 @@ import MapComponent from '../components/Map';
  */
 
 const Home = (props) => {
-
-  // #region map settings stuff
-  const [isMapModalOpen, setMapModalOpen] = useState(false);
-  // for future use
-  const [viewPersonal, setViewPersonal] = useState(false);
-  const [viewFriends, setViewFriends] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  // Open the map settings modal
-  const handleMapSettingsClick = () => {
-    setMapModalOpen(true);
-  };
-
-  // Toggle for Personal Pins
-  const handleTogglePersonal = (isOn) => {
-    setViewPersonal(isOn);
-    console.log('Personal view toggled:', isOn);
-  };
-
-  // Toggle for Friends' Pins
-  const handleToggleFriends = (isOn) => {
-    setViewFriends(isOn);
-    console.log('Friends view toggled:', isOn);
-  };
-
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-    console.log('Searching for:', query);
-    // Implement your map searching logic here
-  };
-
-
-
-  // #endregion
-
-
-  // #region pin creation
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [pins, setPins] = useState([]);
-  const [newPinLocation, setNewPinLocation] = useState({ x: 0, y: 0 });
-
-  // handle clicking on the map to place a new pin
-  const handleMapClick = (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    setNewPinLocation({ x, y });
-    setModalOpen(true);  // open the modal for pin creation
-  };
-
-  // handle creating a new pin from modal submission
-  const handleCreatePin = (description) => {
-    setPins([...pins, { description, x: newPinLocation.x, y: newPinLocation.y }]);
-    setModalOpen(false);  // close modal after creating pin
-  };
-
-  // handle reporting a pin
-  const handleReportPin = (index) => {
-    alert(`Reporting pin ${index}`);
-  };
-
-  // #endregion
-
-  // zoom functionality for map
-  const [scale, setScale] = useState(1);
-  const handleZoom = (event) => {
-    const zoomFactor = event.deltaY < 0 ? 0.1 : -0.1;
-    setScale((prevScale) => Math.max(1, prevScale + zoomFactor));
-  };
-
   return (
     <div className="map-container w-full h-[80vh]">
-
-
       <MapComponent />
-  
-
-      
     </div>
   );
 };
