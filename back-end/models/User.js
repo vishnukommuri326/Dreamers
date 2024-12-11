273f673f9a8bdaf81e8ba56b878307fcf0e80c9c
seokwak <sk9575@nyu.edu>
2024-12-11 18:57:01 -0500

    added notification feature for when a friend is added

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -10,7 +10,16 @@ const userSchema = new mongoose.Schema({
     number: { type: String, default: '' },
     otherSocialMedia: { type: String, default: '' },
     friendsList: { type: [String], default: [] },
-    profilePhoto: { type: String, default: '' }
+    profilePhoto: { type: String, default: '' },
+    notifications: {
+        type: [
+            {
+                message: { type: String, required: true },
+                createdAt: { type: Date, default: Date.now },
+            }
+        ],
+        default: [],
+    }
 });
 
 // Hash password before saving
