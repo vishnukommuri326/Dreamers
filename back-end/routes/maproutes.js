15121ab29f104c75a467ab18f8b6916e47d4f8ef
seokwak <sk9575@nyu.edu>
2024-12-12 11:47:55 -0500

    edit

--- back-end/routes/maproutes.js
+++ back-end/routes/maproutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// TODO: create routes for map view once friends and currentuser routes are complete
-
 router.get('/map'), (req, res) => {
     const { view } = req.query;
     try {
